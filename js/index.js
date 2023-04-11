c6936d132576170c2f9c605d748c8cf379339d56
Vivek <vivek.rbj.up@gmail.com>
2023-04-12 01:21:52 +0530

    fixing link in index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // document.addEventListener("keypress", )
 $(document).keypress(function (event) {
-    location.replace("../html/pingpong.html")
+    location.replace("html/pingpong.html")
 });
 
 $(document).click(function (event) {
-    location.replace("../html/pingpong.html")
+    location.replace("html/pingpong.html")
 });
 
